5722dca9bc34a863405068fa638573ece4acaf28
Ilya <luchinsky99@gmail.com>
2021-12-13 15:11:07 +0600

    [+] add flutter app

---
+++ Flutter/marvel_app/lib/heroes.dart
@@ -0,0 +1,38 @@
+class Heroes {
+final String name;
+final String path;
+
+Heroes({required this.name, required this.path});
+}
+
+class HeroesList {
+final _heroes = [
+Heroes(
+name: 'Carnage',
+path:
+'images/Carnage.jpg'),
+
+Heroes(
+name: 'Doctor Doom',
+path:
+'images/Doctor_Doom.png'),
+
+Heroes(
+name: 'Iron man',
+path:
+'images/Iron_man.jpg'),
+
+Heroes(
+name: 'Spider man',
+path:
+'images/Spider_man.jpg'),
+
+Heroes(
+name: 'Venom',
+path:
+'images/Venom.jpg'),
+
+];
+
+List<Heroes> get() => [..._heroes];
+}
